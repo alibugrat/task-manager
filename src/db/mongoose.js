35212272420b6e3912998458e3584c26ba0838a9
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2022-01-12 14:06:54 +0300

    add mongoose.js

---
+++ src/db/mongoose.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    }
+})
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
