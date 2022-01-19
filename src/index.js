e87e688f47e888b8b5df3281a4585e11ca4a4b5a
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2022-01-19 20:52:10 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routers/user')
-const taskRouter = require('./routers/tasks')
+const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
-
 
 
 app.listen(port, () => {
